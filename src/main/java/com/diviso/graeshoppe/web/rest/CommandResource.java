97993956e75f0c66ac88d97ec03c797640105121
rafeekbytatech <abdul.rafeek@lxisoft.com>
2019-04-25 10:00:58 +0530

    refactored

--- src/main/java/com/diviso/graeshoppe/web/rest/CommandResource.java
+++ src/main/java/com/diviso/graeshoppe/web/rest/CommandResource.java
@@ -13,6 +13,7 @@ import com.diviso.graeshoppe.client.customer.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/api/command")
 public class CommandResource {
@@ -21,30 +22,17 @@ public class CommandResource {
 	@Autowired
 	private CustomerResourceApi customerResourceApi;
 	private final Logger log = LoggerFactory.getLogger(CommandResource.class);
-	
-	
+
 	@PostMapping("/customers/register-customer")
 	public void createCustomer(@RequestBody CustomerAggregator customerAggregator) {
-CustomerDTO customerDTO= new CustomerDTO();
-customerDTO.setName(customerAggregator.getName());
-customerResourceApi.createCustomerUsingPOST(customerDTO);
 
-		/*System.out.println("customerAggregator>>>>>>>>>>>>>>>"+customerAggregator);
-		
-		log.debug("REST request to save customer : {}",  customerAggregator );
-		CustomerDTO customerDTO= new CustomerDTO();
-		ContactDTO contactDTO= new ContactDTO();
+		CustomerDTO customerDTO = new CustomerDTO();
+		ContactDTO contactDTO = new ContactDTO();
 		customerDTO.setName(customerAggregator.getName());
-System.out.println("customerDTO>>>>>>>>>>>>>>>"+customerDTO);*/
-//contactResourceApi.createContactUsingPOST(customerAggregator);
-		
-			
-		//customerDTO.setContactId(resultDTO.getId());
-		/*contactDTO.setMobileNumber(customerAggregator.getMobileNumber());
-		ContactDTO resultDTO=contactResourceApi.createContactUsingPOST(contactDTO).getBody(); 
-		
-		customerResourceApi.createCustomerUsingPOST(customerDTO);*/
-		
+		contactDTO.setMobileNumber(customerAggregator.getMobileNumber());
+		ContactDTO resultDTO = contactResourceApi.createContactUsingPOST(contactDTO).getBody();
+		customerDTO.setContactId(resultDTO.getId());
+		customerResourceApi.createCustomerUsingPOST(customerDTO);
 
 	}
 }
