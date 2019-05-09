9b04ae3236dbbb329ed6db379d54997ec27eaa00
sruthykc <sruthy.k.c@lxisoft.com>
2019-05-09 15:37:59 +0530

    Fixed Sale client Configuration Issues

--- src/main/java/com/diviso/graeshoppe/client/sale/api/UserResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/sale/api/UserResourceApiClient.java
@@ -1,8 +1,9 @@
 package com.diviso.graeshoppe.client.sale.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.diviso.graeshoppe.client.sale.ClientConfiguration;
 
-@FeignClient(name="${sale.name:sale}", url="${sale.url:34.73.191.107:8086/}", configuration = ClientConfiguration.class)
+import com.diviso.graeshoppe.client.sale.SaleClientConfiguration;
+
+@FeignClient(name="${sale.name:sale}", url="${sale.url:34.73.191.107:8086/}", configuration = SaleClientConfiguration.class)
 public interface UserResourceApiClient extends UserResourceApi {
 }
