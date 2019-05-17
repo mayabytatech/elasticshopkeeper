dd49c3bc17af14ae1ce37ecb955a8c7bbe00cdfe
sruthykc <sruthy.k.c@lxisoft.com>
2019-05-17 14:57:08 +0530

    Swagger fixes

--- src/main/java/com/diviso/graeshoppe/client/product/api/LabelResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/product/api/LabelResourceApiClient.java
@@ -1,7 +1,8 @@
 package com.diviso.graeshoppe.client.product.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.diviso.graeshoppe.client.product.ClientConfiguration;
+
+import com.diviso.graeshoppe.client.customer.ClientConfiguration;
 
 @FeignClient(name="${product.name:product}", url="${product.url:34.73.191.107:8084/}", configuration = ClientConfiguration.class)
 public interface LabelResourceApiClient extends LabelResourceApi {
