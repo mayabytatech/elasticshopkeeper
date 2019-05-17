4689c9d63e2ab99683a3f312c0035a26bc3d1d4b
sruthykc <sruthy.k.c@lxisoft.com>
2019-05-17 16:19:26 +0530

    Client configuration conflit fix

--- src/main/java/com/diviso/graeshoppe/client/store/api/StoreResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/store/api/StoreResourceApiClient.java
@@ -1,7 +1,8 @@
 package com.diviso.graeshoppe.client.store.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.diviso.graeshoppe.client.store.ClientConfiguration;
+
+import com.diviso.graeshoppe.client.customer.ClientConfiguration;
 
 @FeignClient(name="${store.name:store}", url="${store.url:34.73.191.107:8071/}", configuration = ClientConfiguration.class)
 public interface StoreResourceApiClient extends StoreResourceApi {
