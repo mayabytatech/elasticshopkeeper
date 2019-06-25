659595d6c884a12e672458048c2985e270a71fa4
mayabytatech <maya.k.k@lxisoft.com>
2019-06-25 14:23:45 +0530

    requestpart replace ment

--- src/main/java/com/diviso/graeshoppe/web/rest/QueryResource.java
+++ src/main/java/com/diviso/graeshoppe/web/rest/QueryResource.java
@@ -362,7 +362,7 @@ public class QueryResource {
 	}
 
 	@PostMapping("/loadProducts")
-	public void loadProducts(@RequestParam("file") MultipartFile file) {
+	public void loadProducts(@RequestPart("file") MultipartFile file) {
 		// upload and save the file then load
 		log.info("::::::::::::::::::file:::::::::::::::::::::: "+ file);
 		loadControllerApi.loadUsingPOST(file);
