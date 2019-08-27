c05838cdc065a9ac5c6df5f53dcd36aa3ff54708
sruthykc <sruthy.k.c@gmail.com>
2019-08-27 14:21:45 +0530

    Fix

--- src/main/java/com/diviso/graeshoppe/client/order/api/AddressResourceApiClient.java
+++ src/main/java/com/diviso/graeshoppe/client/order/api/AddressResourceApiClient.java
@@ -3,6 +3,6 @@ package com.diviso.graeshoppe.client.order.api;
 import org.springframework.cloud.openfeign.FeignClient;
 import com.diviso.graeshoppe.client.order.OrderClientConfiguration;
 
-@FeignClient(name="${order.name:order}", url="${order.url/}", configuration = OrderClientConfiguration.class)
+@FeignClient(name="${order.name:order}", url="${order.url}", configuration = OrderClientConfiguration.class)
 public interface AddressResourceApiClient extends AddressResourceApi {
 }
