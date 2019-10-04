97ea4de155b59485281e9f9b913585c90f9dae32
sruthykc <sruthy.k.c@gmail.com>
2019-10-04 16:48:14 +0530

    fix

--- src/main/java/com/diviso/graeshoppe/client/aggregators/CustomerAggregator.java
+++ src/main/java/com/diviso/graeshoppe/client/aggregators/CustomerAggregator.java
@@ -1,29 +1,39 @@
 package com.diviso.graeshoppe.client.aggregators;
 
 public class CustomerAggregator {
+	
 	private String name;
-	private String mobileNumber;
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
+	private Long mobileNumber;
+	private String reference;
+	private String email;
+	private Long phoneCode;
+	private String searchKey;
+	
+	
+	
 	@Override
 	public String toString() {
-		return String.format("CustomerAggregator [name=%s, mobileNumber=%s]", name, mobileNumber);
+		return "CustomerAggregator [name=" + name + ", mobileNumber=" + mobileNumber + ", reference=" + reference
+				+ ", email=" + email + ", phoneCode=" + phoneCode + ", searchKey=" + searchKey + "]";
 	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
+	
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
 		result = prime * result + ((mobileNumber == null) ? 0 : mobileNumber.hashCode());
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((phoneCode == null) ? 0 : phoneCode.hashCode());
+		result = prime * result + ((reference == null) ? 0 : reference.hashCode());
+		result = prime * result + ((searchKey == null) ? 0 : searchKey.hashCode());
 		return result;
 	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
+
+
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -33,6 +43,11 @@ public class CustomerAggregator {
 		if (getClass() != obj.getClass())
 			return false;
 		CustomerAggregator other = (CustomerAggregator) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
 		if (mobileNumber == null) {
 			if (other.mobileNumber != null)
 				return false;
@@ -43,33 +58,64 @@ public class CustomerAggregator {
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
+		if (phoneCode == null) {
+			if (other.phoneCode != null)
+				return false;
+		} else if (!phoneCode.equals(other.phoneCode))
+			return false;
+		if (reference == null) {
+			if (other.reference != null)
+				return false;
+		} else if (!reference.equals(other.reference))
+			return false;
+		if (searchKey == null) {
+			if (other.searchKey != null)
+				return false;
+		} else if (!searchKey.equals(other.searchKey))
+			return false;
 		return true;
 	}
-	/**
-	 * @return the name
-	 */
+
+
+
 	public String getName() {
 		return name;
 	}
-	/**
-	 * @param name the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-	/**
-	 * @return the mobileNumber
-	 */
-	public String getMobileNumber() {
+	public Long getMobileNumber() {
 		return mobileNumber;
 	}
-	/**
-	 * @param mobileNumber the mobileNumber to set
-	 */
-	public void setMobileNumber(String mobileNumber) {
+	public void setMobileNumber(Long mobileNumber) {
 		this.mobileNumber = mobileNumber;
+	}
+	public String getReference() {
+		return reference;
+	}
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getPhoneCode() {
+		return phoneCode;
+	}
+	public void setPhoneCode(Long phoneCode) {
+		this.phoneCode = phoneCode;
+	}
+	public String getSearchKey() {
+		return searchKey;
+	}
+	public void setSearchKey(String searchKey) {
+		this.searchKey = searchKey;
 	}
 	
 	
+
 	
 }
