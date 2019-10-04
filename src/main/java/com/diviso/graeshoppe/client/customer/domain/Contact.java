f28dc4992c8cd3e03814e5996e97760b9fca9793
sruthykc <sruthy.k.c@gmail.com>
2019-10-04 16:42:49 +0530

    Fix

--- src/main/java/com/diviso/graeshoppe/client/customer/domain/Contact.java
+++ src/main/java/com/diviso/graeshoppe/client/customer/domain/Contact.java
@@ -1,39 +1,45 @@
 package com.diviso.graeshoppe.client.customer.domain;
 
-
-import org.hibernate.annotations.Cache;
-import org.hibernate.annotations.CacheConcurrencyStrategy;
-
-import javax.persistence.*;
-
-import org.springframework.data.elasticsearch.annotations.Document;
-import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.validation.annotation.Validated;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModelProperty;
+
 /**
- * A Contact.
+ * Contact
  */
+@Validated
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-09-28T12:48:05.037111+05:30[Asia/Kolkata]")
 
-@Document(indexName = "contact")
-public class Contact implements Serializable {
+public class Contact   {
+  @JsonProperty("email")
+  private String email = null;
 
-    private static final long serialVersionUID = 1L;
-    
-   
-    private Long id;
+  @JsonProperty("id")
+  private Long id = null;
 
-  
-    private String mobileNumber;
+  @JsonProperty("mobileNumber")
+  private Long mobileNumber = null;
 
-   
-    private String telephone;
-    
-	private String email;
-	
-	  public Contact email(String email) {
+  @JsonProperty("phoneCode")
+  private Long phoneCode = null;
+
+  @JsonProperty("telephone")
+  private Long telephone = null;
+
+  public Contact email(String email) {
     this.email = email;
     return this;
   }
+
+  /**
+   * Get email
+   * @return email
+  **/
+  @ApiModelProperty(value = "")
 
 
   public String getEmail() {
@@ -44,41 +50,86 @@ public class Contact implements Serializable {
     this.email = email;
   }
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
-    public Long getId() {
-        return id;
-    }
+  public Contact id(Long id) {
+    this.id = id;
+    return this;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  /**
+   * Get id
+   * @return id
+  **/
+  @ApiModelProperty(value = "")
 
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
 
-    public Contact mobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-        return this;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getTelephone() {
-        return telephone;
-    }
+  public Contact mobileNumber(Long mobileNumber) {
+    this.mobileNumber = mobileNumber;
+    return this;
+  }
 
-    public Contact telephone(String telephone) {
-        this.telephone = telephone;
-        return this;
-    }
+  /**
+   * Get mobileNumber
+   * @return mobileNumber
+  **/
+  @ApiModelProperty(value = "")
 
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+
+  public Long getMobileNumber() {
+    return mobileNumber;
+  }
+
+  public void setMobileNumber(Long mobileNumber) {
+    this.mobileNumber = mobileNumber;
+  }
+
+  public Contact phoneCode(Long phoneCode) {
+    this.phoneCode = phoneCode;
+    return this;
+  }
+
+  /**
+   * Get phoneCode
+   * @return phoneCode
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Long getPhoneCode() {
+    return phoneCode;
+  }
+
+  public void setPhoneCode(Long phoneCode) {
+    this.phoneCode = phoneCode;
+  }
+
+  public Contact telephone(Long telephone) {
+    this.telephone = telephone;
+    return this;
+  }
+
+  /**
+   * Get telephone
+   * @return telephone
+  **/
+  @ApiModelProperty(value = "")
+
+
+  public Long getTelephone() {
+    return telephone;
+  }
+
+  public void setTelephone(Long telephone) {
+    this.telephone = telephone;
+  }
+
 
   @Override
   public boolean equals(java.lang.Object o) {
@@ -92,20 +143,38 @@ public class Contact implements Serializable {
     return Objects.equals(this.email, contact.email) &&
         Objects.equals(this.id, contact.id) &&
         Objects.equals(this.mobileNumber, contact.mobileNumber) &&
+        Objects.equals(this.phoneCode, contact.phoneCode) &&
         Objects.equals(this.telephone, contact.telephone);
   }
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(email, id, mobileNumber, phoneCode, telephone);
+  }
 
-    @Override
-    public String toString() {
-        return "Contact{" +
-            "id=" + getId() +
-            ", mobileNumber='" + getMobileNumber() + "'" +
-            ", telephone='" + getTelephone() + "'" +
-            "}";
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class Contact {\n");
+    
+    sb.append("    email: ").append(toIndentedString(email)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    mobileNumber: ").append(toIndentedString(mobileNumber)).append("\n");
+    sb.append("    phoneCode: ").append(toIndentedString(phoneCode)).append("\n");
+    sb.append("    telephone: ").append(toIndentedString(telephone)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
     }
+    return o.toString().replace("\n", "\n    ");
+  }
 }
+
